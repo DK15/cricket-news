9196dbb8f87ad3fc97a8f503768e3617c2da3c10
Darshan Kawar <darshankawar@gmail.com>
2019-02-28 22:07:48 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,7 +10,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:cricket_news/main.dart';
 
-// void main() {
+ void main() {
 //   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
 //     // Build our app and trigger a frame.
 //     await tester.pumpWidget(MyApp());
@@ -27,4 +27,4 @@ import 'package:cricket_news/main.dart';
 //     expect(find.text('0'), findsNothing);
 //     expect(find.text('1'), findsOneWidget);
 //   });
-// }
+ }
